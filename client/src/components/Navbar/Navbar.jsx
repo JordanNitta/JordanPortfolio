6fd09d4a55ccb8c35928441b723b9a07d513a79f
JordanNitta <Jordannittarodrigues@gmail.com>
2023-05-27 11:13:26 -0700

    Nav bar almost finished

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useE } from 'react'
 import { Link, NavLink, Navigate, useNavigate } from 'react-router-dom'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 import { motion, AnimatePresence } from "framer-motion";
@@ -12,8 +12,12 @@ const Navbar = () => {
         { title: 'Skills', URL: '/skills' },
         { title: 'Project', URL: '/project' },
         { title: 'Contact', URL: '/contact' },
-
     ]
+
+    const personalLinks = [
+        // {logo: }
+    ]
+
     const handleMenu = () => {
         setShowMenu(!showMenu);
     }
@@ -23,26 +27,37 @@ const Navbar = () => {
     }
 
     return (
-        <nav className='fixed w-full flex justify-between h-[68px] items-center sm:px-4 sm:py-2' >
-            <h3 className='font-style text-main font-semibold text-[20px] cursor-pointer sm:text[20px]'>Jordan Nitta-Rodrigues</h3>
-
-
-            <AiOutlineMenu size={35} className=" text-main cursor-pointer sm:hidden" />
-
-            <div className='justify-center items-center hidden sm:flex' onClick={handleMenu}>
-                <ul className="flex justify-center items-center">
-                    {navLinks.map((link, index) => (
-                        <li key={index}>
-                            <Link to={link.URL} className='text-[19px] text-main px-4 font-style font-medium'>
-                                {link.title}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
+        <nav className='fixed w-full flex justify-between h-[68px] items-center sm:px-4 sm:py-2 z-50' >
+            <h1 className='font-style w-full text-main font-semibold text-[20px] cursor-pointer sm:text[20px]'>Jordan Nitta-Rodrigues</h1>
+            <ul className="hidden sm:flex sm:justify-center sm:items-center">
+                {navLinks.map((link, index) => (
+                    <li key={index}>
+                        <NavLink to={link.URL} className={({isActive}) => isActive ? 'font-style text-primary-color text-[19px] px-4' : 'font-style text-main text-[19px] px-4'}>
+                            {link.title}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
+            <div onClick={handleMenu} className='block '>
+                {!showMenu ? <AiOutlineMenu size={35} className='cursor-pointer sm:hidden md:hidden' /> : <AiOutlineClose size={35} className='cursor-pointer sm:hidden md:hidden' />}
             </div>
+            {showMenu && (
+                <div className='fixed top-0 left-0 h-screen w-full bg-opacity-90 bg-gray-800 flex justify-center items-center sm:hidden md:hidden -z-30' >
+                    <ul className='flex flex-col justify-center items-center' style={{ gap: '0' }} onClick={handleClosedRoutes}>
+                        {navLinks.map((link, index) => (
+                            <li key={index}>
+                                <NavLink to={link.URL} className={({isActive}) => isActive ? 'font-style text-primary-color text-[50px] font-bold' :'font-style text-main text-[60px]'} onClick={handleClosedRoutes}>
+                                    {link.title}
+                                </NavLink>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </nav>
 
     )
 }
+
 
 export default Navbar
