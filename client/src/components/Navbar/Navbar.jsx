b77d7f8d6fcf2cc698d6c15b59a0afb40718562b
JordanNitta <Jordannittarodrigues@gmail.com>
2023-05-27 18:19:15 -0700

    Nav Bar Completed

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { useState, useE } from 'react'
+import { useState, useEffect } from 'react'
 import { Link, NavLink, Navigate, useNavigate } from 'react-router-dom'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
 import { motion, AnimatePresence } from "framer-motion";
 const Navbar = () => {
     const [showMenu, setShowMenu] = useState(false);
@@ -15,7 +16,8 @@ const Navbar = () => {
     ]
 
     const personalLinks = [
-        // {logo: }
+        { logo: FaGithub, URL: 'https://github.com/JordanNitta' },
+        { logo: FaLinkedinIn, URL: 'https://www.linkedin.com/in/jordan-nitta-rodrigues-824857238/' }
     ]
 
     const handleMenu = () => {
@@ -32,25 +34,37 @@ const Navbar = () => {
             <ul className="hidden sm:flex sm:justify-center sm:items-center">
                 {navLinks.map((link, index) => (
                     <li key={index}>
-                        <NavLink to={link.URL} className={({isActive}) => isActive ? 'font-style text-primary-color text-[19px] px-4' : 'font-style text-main text-[19px] px-4'}>
+                        <NavLink to={link.URL} className={({ isActive }) => isActive ? 'font-style text-primary-color text-[19px] px-4' : 'font-style text-main text-[19px] px-4'}>
                             {link.title}
                         </NavLink>
                     </li>
                 ))}
             </ul>
             <div onClick={handleMenu} className='block '>
-                {!showMenu ? <AiOutlineMenu size={35} className='cursor-pointer sm:hidden md:hidden' /> : <AiOutlineClose size={35} className='cursor-pointer sm:hidden md:hidden' />}
+                {!showMenu ? <AiOutlineMenu size={35} className='cursor-pointer text-main sm:hidden md:hidden' /> : <AiOutlineClose size={35} className='cursor-pointer text-main sm:hidden md:hidden' />}
             </div>
             {showMenu && (
                 <div className='fixed top-0 left-0 h-screen w-full bg-opacity-90 bg-gray-800 flex justify-center items-center sm:hidden md:hidden -z-30' >
-                    <ul className='flex flex-col justify-center items-center' style={{ gap: '0' }} onClick={handleClosedRoutes}>
+                    <ul className='flex flex-col justify-center items-center -space-y-4' onClick={handleClosedRoutes}>
                         {navLinks.map((link, index) => (
                             <li key={index}>
-                                <NavLink to={link.URL} className={({isActive}) => isActive ? 'font-style text-primary-color text-[50px] font-bold' :'font-style text-main text-[60px]'} onClick={handleClosedRoutes}>
+                                <NavLink to={link.URL} className={({ isActive }) => isActive ? 'font-style text-primary-color text-[50px] font-semibold hover:text-primary-color transition duration-500' : 'font-style text-main text-[60px] font-semibold hover:text-primary-color transition duration-500'} onClick={handleClosedRoutes}>
                                     {link.title}
                                 </NavLink>
                             </li>
                         ))}
+                        <div className='flex space-x-10'>
+                            {personalLinks.map((link, index) => {
+                                const IconLogo = link.logo;
+                                return (
+                                    <li key={index} className="text-main mt-10  hover:text-primary-color transition duration-500">
+                                        <a href={link.URL}>
+                                            <IconLogo size={25} />
+                                        </a>
+                                    </li>
+                                );
+                            })}
+                        </div>
                     </ul>
                 </div>
             )}
--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -16,8 +16,8 @@ const Main = () => {
 
 
     return (
-        <div className='bg-main-color h-screen flex justify-center items-center box-border overflow-hidden sm:justify-start'>
-            <div className="flex justify-center items-center h-screen sm:ml-60">
+        <div className='bg-main-color h-screen flex justify-center  items-center box-border overflow-hidden sm:justify-start'>
+            <div className="flex justify-center items-center h-screen 0 sm:ml-60">
                 <div className="flex flex-col items-center sm:flex-col sm:items-start sm:justify-start">
                     <motion.h1
                         initial={{ opacity: 0, x: -40 }}
@@ -27,9 +27,11 @@ const Main = () => {
                         Hello, I'm Jordan
                     </motion.h1>
                     <motion.div
+                        className='z-10'
                         initial={{ opacity: 0, x: -40 }}
                         animate={{ opacity: 1, x: 0 }}
                         transition={{ duration: .6, delay: 0.3, ease: 'easeIn' }}>
+                        
                         <TypeAnimation
                             sequence={[
                                 'Front End Developer',
@@ -51,7 +53,7 @@ const Main = () => {
                         initial={{ opacity: 0, x: -40 }}
                         animate={{ opacity: 1, x: 0 }}
                         transition={{ duration: 1, delay: 0.4, ease: 'easeIn'  }}
-                        className='text-primary-color'>Developer</motion.h2>
+                        className='text-primary-color z-10'>Developer</motion.h2>
 
                     {/* Contact resume */}
                     <motion.div 
@@ -60,11 +62,11 @@ const Main = () => {
                         transition={{ duration: 1, delay: 0.5, ease: 'easeIn'  }}
                         className='flex item-center justify-between w-[327px] h-[50px] mt-1'>
                         <button
-                            className='rounded main-radius h-[100%] w-main-button-width relative bg-primary-color' >
+                            className='rounded main-radius h-[100%] w-main-button-width relative bg-primary-color z-10' >
                             <Link to="#" className='text-main-color font-semibold font-style text-medium tracking-tighter'>Contact Me</Link>
                         </button>
 
-                        <button className='h-[50px] w-main-button-width border-primary-color rounded-md border-2  flex justify-center items-center'>
+                        <button className='h-[50px] w-main-button-width border-primary-color rounded-md border-2  flex justify-center items-center z-10'>
                             <Link to='#' className='text-medium font-style tracking-tighter text-primary-color'>View Resume</Link>
                         </button>
                     </motion.div>
@@ -75,22 +77,21 @@ const Main = () => {
                         animate={{ opacity: 1, x: 0 }}
                         transition={{ duration: 1, delay: 0.6, ease: 'easeIn'  }}
                         className='flex justify-between item-center w-[150px] sm:absolute bottom-[40px]'>
-                        <button className='flex items-center justify-center w-[50px] h-[50px] rounded-full border-[2px] border-primary-color'>
+                        <button className='flex items-center justify-center w-[50px] h-[50px] rounded-full border-[2px] border-primary-color z-10'>
                             <a href="" className='flex items-center justify-center w-[150px] h-[100%] '>
                                 <FaGithub className='text-primary-color text-[25px]' />
                             </a>
                         </button>
-                        <button className='flex items-center w-[50px] h-[50px] rounded-full border-[2px] border-primary-color '>
+                        <button className='flex items-center w-[50px] h-[50px] rounded-full border-[2px] border-primary-color z-10'>
                             <a href="" className='flex items-center justify-center w-[150px] h-[100%]'>
                                 <FaLinkedinIn className='text-primary-color text-[25px]' />
                             </a>
                         </button>
                     </motion.div>
-                    <div className>
-                        <img src={ProfilePic} className='absolute bottom-0 right-0 h-[600px] mix-blend-overlay filter brightness-125 contrast-60 saturate-20 overflow-hidden'
-                        />
+                    <div className=''>
+                        <img src={ProfilePic} alt='Profile pic'className='absolute bottom-0 right-0 h-[600px] mix-blend-overlay filter brightness-125 contrast-60 saturate-20 overflow-hidden '
+                    />
                     </div>
-                    
                 </div>
             </div>
         </div>
