ad55ae9a189ef3941ed2291964491ebd812b9204
JordanNitta <Jordannittarodrigues@gmail.com>
2023-06-12 13:15:34 -0700

    fixing changes

--- src/components/ProjectMade/Project.jsx
+++ src/components/ProjectMade/Project.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 const Project = () => {
     return (
         <>
-            <div id="project" className='bg-main-color w-full flex justify-center items-center h-screen border-b-[1px] border-primary-color'>
-                <div className='flex-col justify-center items-center w-[90%] lg:flex px-20 lg:flex-row lg:w-[88%]'>
+            <div id="project" className='bg-main-color w-full flex justify-center items-center h-screen sm:border-b-[1px] border-primary-color'>
+                <div className='flex-col justify-center items-center sm:w-[90%] lg:flex px-20 lg:flex-row lg:w-[88%]'>
                     <div className='flex justify-center lg:w-full'>
                         <img src={StarbucksProject} alt="Starbucks Project" className='h-[200px] md:w-[500px] md:h-[320px] rounded-md ' />
                     </div>
                     <div className='flex flex-col items-center justify-center mt-8 sm:mt-0 '>
-                        <h4 className='text-[30px] text-main font-semibold font-style'>Starbucks Clone</h4>
+                        <h4 className='text-[20px] sm:text-[30px] text-main font-semibold font-style'>Starbucks Clone</h4>
                         <h4 className='text-main text-[20px] pt-2 font-semibold font-style'>Full Stack Development</h4>
                         <p className='text-center text-main pt-2 text-[17px] lg:w-[70%] font-style'>Collaborated with <a className="underline"href="https://www.abrardev.com/">Abrar</a> to develop a Starbucks Clone application where users can order and create an account utilizing React, MongoDB, Tailwind CSS.</p>
                         <div className='flex space-x-10 w-[300px] justify-center'>
