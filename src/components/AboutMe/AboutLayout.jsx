851767ab2644d06d258c651e3d6d3192b833637a
JordanNitta <Jordannittarodrigues@gmail.com>
2023-06-12 12:20:13 -0700

    Fixing changes

--- src/components/AboutMe/AboutLayout.jsx
+++ src/components/AboutMe/AboutLayout.jsx
@@ -69,7 +69,7 @@ const AboutLayout = () => {
                     <div className='flex justify-center items-center'>
                         <img src={ProfilePic} alt='Profile pic' className='absolute -bottom-[775px] right-50 left-50 h-[300px] mix-blend-overlay filter brightness-125 contrast-60 saturate-20  
                         md:absolute md:-bottom-[775px] md:left-0 md:h-[400px] md:mix-blend-overlay contrast-60 saturate-20 overflow-hidden lg:absolute lg:-bottom-[775px] lg:left-0 lg:h-[500px]
-                        xl:absolute xl:-bottom-[775px] xl:left-0 xl:h-[600px]'
+                        xl:absolute xl:-bottom-[775px] xl:left-0 xl:h-[600px] xl:w-[550px]'
                         />
                     </div>
                     <div className='flex flex-col items-center justify-center mb-[200px] w-[90%] md:justify-end md:items-center md:w-[80%] md:mb-[200px] lg:mb-0 lg:justify-start lg:items-start lg:w-[50%] '>
